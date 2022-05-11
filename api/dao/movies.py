bef956dc7964eac3fb7da3fb373d0d81cd6b6e92
Ali Elbashir <alielbashiir@gmail.com>
2022-05-11 03:02:23 +0300

    Implement get all movies

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -1,13 +1,13 @@
-from api.data import popular, goodfellas
-
+from api.data import goodfellas, popular
 from api.exceptions.notfound import NotFoundException
-from api.data import popular
+
 
 class MovieDAO:
     """
     The constructor expects an instance of the Neo4j Driver, which will be
     used to interact with Neo4j.
     """
+
     def __init__(self, driver):
         self.driver = driver
 
@@ -20,9 +20,24 @@ class MovieDAO:
      signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::all[]
+
     def all(self, sort, order, limit=6, skip=0, user_id=None):
-        # TODO: Get list from movies from Neo4j
-        return popular
+        def get_movies(tx, sort, order, limit, skip, user_id):
+            """unit of work"""
+            cypher = """
+            MATCH (m:Movie)
+            WHERE exists(m.`{0}`)
+            RETURN m {{ .* }} AS movie
+            ORDER BY m.`{0}` {1}
+            SKIP $skip  
+            LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id)
+            return [row.value("movie") for row in result]
+        with self.driver.session() as session:
+            return session.read_transaction(get_movies, sort, order, limit, skip, user_id)
+
     # end::all[]
 
     """
@@ -38,6 +53,7 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::getByGenre[]
+
     def get_by_genre(self, name, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies in a Genre
         # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
@@ -59,6 +75,7 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::getForActor[]
+
     def get_for_actor(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies for an Actor
         # TODO: The Cypher string will be formated so remember to escape the braces: {{tmdbId: $id}}
@@ -80,6 +97,7 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::getForDirector[]
+
     def get_for_director(self, id, sort='title', order='ASC', limit=6, skip=0, user_id=None):
         # TODO: Get Movies directed by a Person
         # TODO: The Cypher string will be formated so remember to escape the braces: {{name: $name}}
@@ -98,6 +116,7 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::findById[]
+
     def find_by_id(self, id, user_id=None):
         # TODO: Find a movie by its ID
         # MATCH (m:Movie {tmdbId: $id})
@@ -119,18 +138,19 @@ class MovieDAO:
     signify whether the user has added the movie to their "My Favorites" list.
     """
     # tag::getSimilarMovies[]
+
     def get_similar_movies(self, id, limit=6, skip=0, user_id=None):
         # TODO: Get similar movies from Neo4j
 
         return popular[skip:limit]
     # end::getSimilarMovies[]
 
-
     """
     This function should return a list of tmdbId properties for the movies that
     the user has added to their 'My Favorites' list.
     """
     # tag::getUserFavorites[]
+
     def get_user_favorites(self, tx, user_id):
         return []
     # end::getUserFavorites[]
--- api/neo4j.py
+++ api/neo4j.py
@@ -10,9 +10,11 @@ Initiate the Neo4j Driver
 # tag::initDriver[]
 def init_driver(uri, username, password):
     # TODO: Create an instance of the driver here
-    current_app.driver = None
 
-    return None
+    current_app.driver = GraphDatabase().driver(uri, auth=(username, password))
+    current_app.driver.verify_connectivity()
+
+    return current_app.driver
 # end::initDriver[]
 
 
