8a21ff9de92280298b565a8bdaa28408805ce2f0
Adam Cowley <adam@adamcowley.co.uk>
2022-09-26 12:50:27 +0100

    Completed challenge: Adding The Favorite Flag

--- api/dao/movies.py
+++ api/dao/movies.py
@@ -26,18 +26,24 @@ class MovieDAO:
         def get_movies(tx, sort, order, limit, skip, user_id):
         # end::get_movies[]
             # tag::allcypher[]
+            # Get User favorites
+            favorites = self.get_user_favorites(tx, user_id)
+
             # Define the cypher statement
             cypher = """
                 MATCH (m:Movie)
                 WHERE m.`{0}` IS NOT NULL
-                RETURN m {{ .* }} AS movie
+                RETURN m {{
+                    .*,
+                    favorite: m.tmdbId IN $favorites
+                }} AS movie
                 ORDER BY m.`{0}` {1}
                 SKIP $skip
                 LIMIT $limit
             """.format(sort, order)
 
             # Run the statement within the transaction passed as the first argument
-            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id)
+            result = tx.run(cypher, limit=limit, skip=skip, user_id=user_id, favorites=favorites)
             # end::allcypher[]
 
             # tag::allmovies[]
@@ -160,5 +166,13 @@ class MovieDAO:
     """
     # tag::getUserFavorites[]
     def get_user_favorites(self, tx, user_id):
-        return []
+        if user_id == None:
+            return []
+
+        result = tx.run("""
+            MATCH (u:User {userId: $userId})-[:HAS_FAVORITE]->(m)
+            RETURN m.tmdbId AS id
+        """, userId=user_id)
+
+        return [ record.get("id") for record in result ]
     # end::getUserFavorites[]
