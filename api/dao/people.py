718070309d25117b6463f0e5a913c47cb2452744
Adam Cowley <adam@adamcowley.co.uk>
2022-09-26 14:47:31 +0100

    Completed challenge: Listing People

--- api/dao/people.py
+++ api/dao/people.py
@@ -21,11 +21,27 @@ class PeopleDAO:
     """
     # tag::all[]
     def all(self, q, sort = 'name', order = 'ASC', limit = 6, skip = 0):
-        # TODO: Get a list of people from the database
-        # TODO: Remember to use double braces to replace the braces in the Cypher query {{ }}
+        # Get a list of people from the database
+        def get_all_people(tx, q, sort, order, limit, skip):
+            cypher = "MATCH (p:Person) "
 
-        return people[skip:limit]
+            # If q is set, use it to filter on the name property
+            if q is not None:
+                cypher += "WHERE p.name CONTAINS $q"
 
+            cypher += """
+            RETURN p {{ .* }} AS person
+            ORDER BY p.`{0}` {1}
+            SKIP $skip
+            LIMIT $limit
+            """.format(sort, order)
+
+            result = tx.run(cypher, q=q, sort=sort, order=order, limit=limit, skip=skip)
+
+            return [ row.get("person") for row in result ]
+
+        with self.driver.session() as session:
+            return session.execute_read(get_all_people, q, sort, order, limit, skip)
     # end::all[]
 
     """
